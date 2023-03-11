6da0bc71ccfd70994e27bd0feb12a8313548b5be
Franco Galluccio <francoivangalluccio@gmail.com>
2023-03-11 11:08:43 -0300

    Logica de ItemDetailContainer

--- src/components/ItemDetailContainer/index.jsx
+++ src/components/ItemDetailContainer/index.jsx
@@ -1,27 +1,39 @@
 import products from "../../mocks/products";
 import { useEffect, useState } from "react";
 import { Container } from "@mui/system";
-import ItemDetail from "../ItemDetail/ItemDetail";
+import ItemDetail from "../ItemDetail";
 
 export default function ItemDetailContainer({ categoryId, isCategoryRoute }) {
   const [product, setProduct] = useState(null);
-  useEffect(() => {
-    const productPromise = new Promise((resolve, reject) =>
-      setTimeout(() => resolve(products), 2000)
-    );
 
-    productPromise
-      .then((response) => {
-        if (isCategoryRoute) {
-          const productsFiltered = response.find(
-            (producto) => producto.id === categoryId
-          );
-          setProduct(productsFiltered);
-        } else {
-          setProduct(response);
-        }
-      })
-      .catch((err) => console.log(err));
+  const getProduct = (id) => {
+    return new Promise((resolve) => {
+      const productoFiltrado = products.find((p) => p.id === parseInt(id));
+      setTimeout(() => {
+        resolve(productoFiltrado);
+      }, 1000);
+    });
+  };
+
+  useEffect(() => {
+    getProduct(categoryId).then((item) => {
+      setProduct(item);
+    });
+    // const productPromise = new Promise((resolve, reject) =>
+    //   setTimeout(() => resolve(products), 2000)
+    // );
+    // productPromise
+    //   .then((response) => {
+    //     if (isCategoryRoute) {
+    //       const productsFiltered = response.find(
+    //         (producto) => producto.id === categoryId
+    //       );
+    //       setProduct(productsFiltered);
+    //     } else {
+    //       setProduct(response);
+    //     }
+    //   })
+    //   .catch((err) => console.log(err));
   }, [categoryId]);
 
   return (
