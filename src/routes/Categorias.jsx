d45fe73205e5889ac4ac0af2df6046d2d1784f6c
Franco Galluccio <francoivangalluccio@gmail.com>
2023-04-05 20:06:15 -0300

    Correcion de diseño en Dark Mode. Agregado los ItemsCounts en cada Card y en detalle del Carrito

--- src/routes/Categorias.jsx
+++ src/routes/Categorias.jsx
@@ -1,27 +1,23 @@
-import { Container } from "@mui/material/";
-import ItemListContainer from "../components/ItemListContainer";
-import { useParams } from "react-router-dom";
-import Footer from "../components/Footer";
+import { Container } from '@mui/material/';
+import ItemListContainer from '../components/ItemListContainer';
+import { useParams } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 function Categorias() {
-  const params = useParams();
-  const isCategoryRoute = Boolean(params.id);
+	const params = useParams();
+	const isCategoryRoute = Boolean(params.id);
 
-  return (
-    <>
-      <Container
-        maxWidth="xl"
-        disableGutters
-        sx={{ height: "100vh" }}
-      >
-        <ItemListContainer
-          isCategoryRoute={isCategoryRoute}
-          categoryId={params.id}
-        />
-      </Container>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Container maxWidth='xl' disableGutters>
+				<ItemListContainer
+					isCategoryRoute={isCategoryRoute}
+					categoryId={params.id}
+				/>
+			</Container>
+			<Footer />
+		</>
+	);
 }
 
 export default Categorias;
